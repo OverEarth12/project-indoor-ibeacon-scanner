68c54be79ad64dd11fcb68624a36deb60eec60eb
siwakorn2544 <56427172+it62070180@users.noreply.github.com>
2023-05-21 02:54:06 +0700

    Not best i guess that all i can do

--- app/src/main/java/org/altbeacon/beaconreference/MainActivity.kt
+++ app/src/main/java/org/altbeacon/beaconreference/MainActivity.kt
@@ -1,7 +1,9 @@
 package org.altbeacon.beaconreference
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -12,11 +14,13 @@ import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import org.altbeacon.beacon.*
+import org.altbeacon.beaconreference.databinding.ActivityMainBinding
 import java.io.IOException
 import java.net.URL
 
@@ -46,10 +50,26 @@ class MainActivity : AppCompatActivity() {
     private var sentData: Boolean = false;
     private var scanner = "";
 
+    private lateinit var binding: ActivityMainBinding
+
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.off_Line -> replaceFragment(offLine())
+                R.id.on_Line -> replaceFragment(onLine())
+                R.id.Add_Persons -> replaceFragment(Persons())
+            }
+        }
+//        setContentView(R.layout.activity_main)
         beaconReferenceApplication = application as BeaconReferenceApplication
 
         // Set up a Live Data observer for beacon data
@@ -86,11 +106,9 @@ class MainActivity : AppCompatActivity() {
         Button1.isEnabled = true
         scannerId.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -156,6 +174,13 @@ class MainActivity : AppCompatActivity() {
             registerBeacon()
         }
 
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 
     override fun onPause() {
